bed9448cf058c703cfd4e91965195549a9496fa4
tluwun <shylgacharsen345@gmail.com>
2025-07-01 14:55:37 +0300

    update configs

---
+++ configs/.config/cmus/lib.pl
@@ -0,0 +1,5 @@
+/home/tluwun/Music/Slipknot - Psychosocial.mp3
+/home/tluwun/Music/Rammstein - Sonne.mp3
+/home/tluwun/Music/Slipknot - People = Shit.mp3
+/home/tluwun/Music/Slipknot - Duality.mp3
+/home/tluwun/Music/Rammstein - Du hast.mp3
