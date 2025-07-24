e61830fd1f5b8c293182b0969bdc5c2a9e33a50f
Tluwun <iwantbeprivate@tutamail.com>
2025-07-25 00:05:01 +0300

    delete some info for me

--- configs/cmus/lib.pl
+++
@@ -1,8 +0,0 @@
-/home/tluwun/Music/Slipknot - Psychososial.mp3
-/home/tluwun/Music/Yes-Roundabout.mp3
-/home/tluwun/Music/Mood Indigo.mp3
-/home/tluwun/Music/9958-Take-the-A-train.mp3
-/home/tluwun/Music/Book Bag.mp3
-/home/tluwun/Music/01 Move.mp3
-/home/tluwun/Music/Ramstien - Sonne.mp3
-/home/tluwun/Music/Rammstein - Du hast.mp3
