8b10c7a349d61942b22f00aba711d7d3a4ff8dda
Tluwun <iwantbeprivate@tutamail.com>
2025-08-16 13:46:11 +0300

    delete .config in configs

--- configs/.config/cmus/lib.pl
+++
@@ -1,5 +0,0 @@
-/home/tluwun/Music/Slipknot - Psychosocial.mp3
-/home/tluwun/Music/Rammstein - Sonne.mp3
-/home/tluwun/Music/Slipknot - People = Shit.mp3
-/home/tluwun/Music/Slipknot - Duality.mp3
-/home/tluwun/Music/Rammstein - Du hast.mp3
